20e5c0776c301ec3623c07c779ccef5ca50ca1a2
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-05-27 20:44:12 +0100

    Initial Commit

---
+++ testCustomTabView/testCustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCustomTabViewApp.swift
+//  testCustomTabView
+//
+//  Created by Danjuma Nasiru on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct testCustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
