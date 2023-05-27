368deb7efcf6c13452636271dbaee5b9aa74c88d
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-05-27 20:44:57 +0100

    Initial Commit

---
+++ testSwiftUICharts/testSwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIChartsApp.swift
+//  testSwiftUICharts
+//
+//  Created by Danjuma Nasiru on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
